c2717f0da3ffb95e6c9c4317d6c8dd7b9fd52944
vfarina <vfarina@ATAWAY-AR323>
2023-09-18 00:33:48 -0300

    Add project files.

---
+++ CRUD/Controllers/CustomersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers/index
+        public ActionResult Index()
+        {
+            using (DBModels db = new DBModels())
+            {
+                var vm = db.Customers.ToList();
+                return View(vm);
+            }
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Customers.Where(x => x.CustomerID == id).FirstOrDefault()); ;
+            }
+               
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (DBModels db = new DBModels())
+                {
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                }
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                var vm = db.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
+                return View(vm); 
+            }
+                
+        }
+
+        // POST: Customers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (DBModels db = new DBModels())
+                {
+                    db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Customers.Where(x => x.CustomerID == id).FirstOrDefault()); ;
+            }
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Customer customer)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (DBModels db = new DBModels())
+                {
+                    customer = db.Customers.Where(x => x.CustomerID == id).FirstOrDefault(); ;
+                    db.Customers.Remove(customer);
+                    db.SaveChanges();
+                }
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
